72b1c713aa80a0934638ffc59f4bf10bfafb4330
Jason Mimick <jmimick@gmail.com>
2021-05-13 13:37:23 -0400

    fix db refs

--- app.py
+++ app.py
@@ -153,8 +153,8 @@ class UpdateStudentModel(BaseModel):
 @app.post("/", response_description="Add new student", response_model=StudentModel)
 async def create_student(student: StudentModel = Body(...)):
     student = jsonable_encoder(student)
-    new_student = await db["students"].insert_one(student)
-    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
+    new_student = await db.college["students"].insert_one(student)
+    created_student = await db.college["students"].find_one({"_id": new_student.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
 
 
@@ -166,9 +166,10 @@ async def healthcheck():
 @app.get(
     "/students", response_description="List all students", response_model=List[StudentModel]
 )
+
 async def list_students():
     students = await db.college["students"].find().to_list(1000)
-    #students = list(db["students"].find().limit(1000))
+    #students = list(db.college["students"].find().limit(1000))
     print(f"students={students}")
     return students
 
@@ -177,7 +178,7 @@ async def list_students():
     "/{id}", response_description="Get a single student", response_model=StudentModel
 )
 async def show_student(id: str):
-    if (student := await db["students"].find_one({"_id": id})) is not None:
+    if (student := await db.college["students"].find_one({"_id": id})) is not None:
         return student
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
@@ -188,15 +189,15 @@ async def update_student(id: str, student: UpdateStudentModel = Body(...)):
     student = {k: v for k, v in student.dict().items() if v is not None}
 
     if len(student) >= 1:
-        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
+        update_result = await db.college["students"].update_one({"_id": id}, {"$set": student})
 
         if update_result.modified_count == 1:
             if (
-                updated_student := await db["students"].find_one({"_id": id})
+                updated_student := await db.college["students"].find_one({"_id": id})
             ) is not None:
                 return updated_student
 
-    if (existing_student := await db["students"].find_one({"_id": id})) is not None:
+    if (existing_student := await db.college["students"].find_one({"_id": id})) is not None:
         return existing_student
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
@@ -204,7 +205,7 @@ async def update_student(id: str, student: UpdateStudentModel = Body(...)):
 
 @app.delete("/{id}", response_description="Delete a student")
 async def delete_student(id: str):
-    delete_result = await db["students"].delete_one({"_id": id})
+    delete_result = await db.college["students"].delete_one({"_id": id})
 
     if delete_result.deleted_count == 1:
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
