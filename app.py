bc6caafe0a894c8076e11bbf6630fee0e5b90588
Rob Young <ryoung@mbopartners.com>
2021-07-15 13:05:33 -0400

    Return str version of BSON on deserializer
    Returning an ObjectId causes 500 error
    > ValueError: [TypeError("'ObjectId' object is not iterable"), TypeError('vars() argument must have __dict__ attribute')]

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class PyObjectId(ObjectId):
     def validate(cls, v):
         if not ObjectId.is_valid(v):
             raise ValueError("Invalid objectid")
-        return ObjectId(v)
+        return str(v)
 
     @classmethod
     def __modify_schema__(cls, field_schema):
