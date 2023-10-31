b15f0db3619c013709dcea6664227d6f45cfaccd
Judy2k <judy@judy.co.uk>
2023-10-31 14:26:00 +0000

    Update code to work with FastAPI 0.104/Pydantic 2.4

--- app.py
+++ app.py
@@ -2,82 +2,81 @@ import os
 from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import ConfigDict, BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
 from bson import ObjectId
 from typing import Optional, List
+from typing_extensions import Annotated
+from pymongo import ReturnDocument
 import motor.motor_asyncio
+
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.college
 
-
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
 class StudentModel(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str = Field(...)
     email: EmailStr = Field(...)
     course: str = Field(...)
     gpa: float = Field(..., le=4.0)
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
             "example": {
                 "name": "Jane Doe",
                 "email": "jdoe@example.com",
                 "course": "Experiments, Science, and Fashion in Nanophotonics",
                 "gpa": "3.0",
             }
-        }
+        },
+    )
 
 
 class UpdateStudentModel(BaseModel):
-    name: Optional[str]
-    email: Optional[EmailStr]
-    course: Optional[str]
-    gpa: Optional[float]
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    course: Optional[str] = None
+    gpa: Optional[float] = None
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str},
+        json_schema_extra={
             "example": {
                 "name": "Jane Doe",
                 "email": "jdoe@example.com",
                 "course": "Experiments, Science, and Fashion in Nanophotonics",
                 "gpa": "3.0",
             }
-        }
+        },
+    )
 
 
-@app.post("/", response_description="Add new student", response_model=StudentModel)
+@app.post(
+    "/api/",
+    response_description="Add new student",
+    response_model=StudentModel,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
 async def create_student(student: StudentModel = Body(...)):
-    student = jsonable_encoder(student)
-    new_student = await db["students"].insert_one(student)
+    new_student = await db["students"].insert_one(
+        student.model_dump(by_alias=True, exclude=["id"])
+    )
     created_student = await db["students"].find_one({"_id": new_student.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+    return created_student
 
 
 @app.get(
-    "/", response_description="List all students", response_model=List[StudentModel]
+    "/api/",
+    response_description="List all students",
+    response_model=List[StudentModel],
+    response_model_by_alias=False,
 )
 async def list_students():
     students = await db["students"].find().to_list(1000)
@@ -85,37 +84,50 @@ async def list_students():
 
 
 @app.get(
-    "/{id}", response_description="Get a single student", response_model=StudentModel
+    "/api/{id}",
+    response_description="Get a single student",
+    response_model=StudentModel,
+    response_model_by_alias=False,
 )
 async def show_student(id: str):
-    if (student := await db["students"].find_one({"_id": id})) is not None:
+    if (student := await db["students"].find_one({"_id": ObjectId(id)})) is not None:
         return student
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
 
 
-@app.put("/{id}", response_description="Update a student", response_model=StudentModel)
+@app.put(
+    "/api/{id}",
+    response_description="Update a student",
+    response_model=StudentModel,
+    response_model_by_alias=False,
+)
 async def update_student(id: str, student: UpdateStudentModel = Body(...)):
-    student = {k: v for k, v in student.dict().items() if v is not None}
+    student = {
+        k: v for k, v in student.model_dump(by_alias=True).items() if v is not None
+    }
 
     if len(student) >= 1:
-        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
+        update_result = await db["students"].find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": student},
+            return_document=ReturnDocument.AFTER,
+        )
+        if update_result is not None:
+            return update_result
+        else:
+            raise HTTPException(status_code=404, detail=f"Student {id} not found")
 
-        if update_result.modified_count == 1:
-            if (
-                updated_student := await db["students"].find_one({"_id": id})
-            ) is not None:
-                return updated_student
-
+    # The update is empty, but we should still return the matching document:
     if (existing_student := await db["students"].find_one({"_id": id})) is not None:
         return existing_student
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
 
 
-@app.delete("/{id}", response_description="Delete a student")
+@app.delete("/api/{id}", response_description="Delete a student")
 async def delete_student(id: str):
-    delete_result = await db["students"].delete_one({"_id": id})
+    delete_result = await db["students"].delete_one({"_id": ObjectId(id)})
 
     if delete_result.deleted_count == 1:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
